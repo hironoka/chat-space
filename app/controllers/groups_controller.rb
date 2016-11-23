fd1e6223bca392ee82fb7e533a22929382ae0074
hirono <hotarak@gmail.com>
2016-11-23 18:03:37 +0900

    add new, create, edit, update actions to groups controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,17 @@
 class GroupsController < ApplicationController
+
+  before_action :set_group, only: [:edit, :update]
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
 end
