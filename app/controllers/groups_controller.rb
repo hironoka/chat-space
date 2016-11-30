d3c495c3b725d12a276bdae4aadc8e267ec713f0
hirono <hotarak@gmail.com>
2016-11-30 16:44:00 +0900

    add new and crate action to groups_controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,15 +1,35 @@
 class GroupsController < ApplicationController
 
+  def index
+    @groups = Group.all
+  end
+
   def new
+    @group = Group.new
   end
 
   def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to :root
+    else
+      render new_group_path
+    end
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(
+    :name,
+    {user_ids: []}
+    )
   end
 
 end
