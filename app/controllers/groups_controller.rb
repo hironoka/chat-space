cad8a769ca7ed91f09c8c8ef39994fd63c7cd250
hirono <hotarak@gmail.com>
2016-12-03 17:32:32 +0900

    edit redirect path

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,6 @@
 class GroupsController < ApplicationController
 
-  before_action :set_group, pnly: [:edit, :update]
+  before_action :set_group, only: [:edit, :update]
 
   def index
     @groups = current_user.groups
@@ -13,7 +13,7 @@ class GroupsController < ApplicationController
   def create
     @group = Group.new(group_params)
     if @group.save
-      redirect_to :root
+      redirect_to  group_messages_path(@group)
     else
       render new_group_path
     end
@@ -24,7 +24,7 @@ class GroupsController < ApplicationController
 
   def update
     if @group.update(group_params)
-      redirect_to :root
+      redirect_to group_messages_path(@group)
     else
       render edit_group_path
     end
