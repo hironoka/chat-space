bb2de2c9297cfcdc594e7f6ae250da1b3b0ba0ca
hirono <hotarak@gmail.com>
2016-12-05 22:24:08 +0900

    edit controller and define message_params

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,6 +5,17 @@ class MessagesController < ApplicationController
   def index
     @groups = current_user.groups
     @users = @group.users
+    @messages = @group.messages
+    @message = Message.new
+  end
+
+  def create
+    @message = Message.new(message_params)
+    if @message.save
+      redirect_to :back
+    else
+      redirect_to :back, alert: "メッセージを入力してください"
+    end
   end
 
   private
@@ -13,4 +24,12 @@ class MessagesController < ApplicationController
     @group = Group.find(params[:group_id])
   end
 
+  def message_params
+    params.require(:message).permit(
+    :body,
+    :image,
+    :user_id,
+    :group_id
+    )
+  end
 end
