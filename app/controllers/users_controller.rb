b26e653a26463471236a25440fad5cf82bbbc500
hirono <hotarak@gmail.com>
2016-12-28 12:33:01 +0900

    add index action to users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,11 @@
 class UsersController < ApplicationController
+
+  def index
+    @all_users = User.where('name LIKE(?)', "#{params[:name]}%")
+    @users = @all_users.where.not(id: current_user.id)
+    respond_to do |format|
+      format.json { render json: @users }
+    end
+  end
+
 end
