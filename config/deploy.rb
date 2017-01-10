f202db77fdb80454c489b9b27c60fb2736af342a
hirono <hotarak@gmail.com>
2017-01-10 19:17:12 +0900

    edit(create) deploy.rb ここには、production環境、stading環境どちらにも当てはまる設定を記述する

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,23 +1,64 @@
-# config valid only for current version of Capistrano
-lock "3.7.1"
+# capistranoのバージョン固定
+lock '3.4.0'
 
-set :application, 'rails-capistrano-test'
-set :repo_url, 'git@github.com:chat-space.git'
+set :application, 'chat-space'
 
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+# cloneするgitのレポジトリ
+set :repo_url, 'git@github.com:hotarak/chat-space.git'
 
-set :rbenv_type, :user
+# deployするブランチ。デフォルトはmasterなのでなくても可。
+set :branch, 'master'
+
+# deploy先のディレクトリ
+set :deploy_to, '/home/hiro/chat-space'
+
+# シンボリックリンクをはるファイル。(※後述)
+set :linked_files, fetch(:linked_files, []).push('config/secrets.yml')
+
+# シンボリックリンクをはるフォルダ。(※後述)
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+
+# 保持するバージョンの個数(※後述)
+set :keep_releases, 5
+
+# rubyのバージョン（ruby -vコマンドで確認し、ご自身が使用しているバージョンを指定してください。）
 set :rbenv_ruby, '2.3.1'
 
-set :ssh_options, auth_methods: ['publickey'],
-                  keys: '/Users/kawashimahirono/.ssh/key_pair_2.pem'
+#出力するログのレベル。
+set :log_level, :debug
 
-set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
-set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
-
-after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
+  desc 'Restart application'
   task :restart do
     invoke 'unicorn:restart'
+  end
+
+  desc 'Create database'
+  task :db_create do
+    on roles(:db) do |host|
+      with rails_env: fetch(:rails_env) do
+        within current_path do
+          execute :bundle, :exec, :rake, 'db:create'
+        end
+      end
+    end
+  end
+
+  desc 'Run seed'
+  task :seed do
+    on roles(:app) do
+      with rails_env: fetch(:rails_env) do
+        within current_path do
+          execute :bundle, :exec, :rake, 'db:seed'
+        end
+      end
+    end
+  end
+
+  after :publishing, :restart
+
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+    end
   end
 end
