8f252fa135df0e4e2751fece109e36e587db3b55
hirono <hotarak@gmail.com>
2017-01-10 20:38:47 +0900

    change default env

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,7 @@
 server '35.166.45.73', user: 'hiro', roles: %w{app db web}
 
 set :ssh_options,{ keys: %w(~/.ssh/practice_key_rsa) }
+set :default_env, {
+  ACCESS_KEY_ID: ENV["ACCESS_KEY_ID"],
+  SECRET_ACCESS_KEY: ENV["SECRET_ACCESS_KEY"]
+}
